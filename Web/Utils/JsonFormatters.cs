2782d3ebc0b229b2ab8ae9a1f8a4980274263038
Iount Neagos <ionutneagos@yahoo.com>
2022-04-05 15:06:05 +0300

    Finished app identity configuration

---
+++ Web/Utils/JsonFormatters.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Options;
+
+namespace Web.Utils
+{
+    public static class JsonPatchFormatter
+    {
+        public static NewtonsoftJsonPatchInputFormatter GetJsonPatchInputFormatter()
+        {
+            var builder = new ServiceCollection()
+                .AddLogging()
+                .AddMvc()
+                .AddNewtonsoftJson()
+                .Services.BuildServiceProvider();
+
+            return builder
+                .GetRequiredService<IOptions<MvcOptions>>()
+                .Value
+                .InputFormatters
+                .OfType<NewtonsoftJsonPatchInputFormatter>()
+                .First();
+        }
+    }
+}
